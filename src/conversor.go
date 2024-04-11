123759ba56bcc714b57a045073c470ad2a12414c
Thiffany <thiffany.ariane@veri.com.br>
2024-04-11 09:10:41 -0300

    feat: Conversor de Kelvin para Celsius

---
+++ src/conversor.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var pontoEbulicaoKelvin = 373
+
+func main() {
+	var celsius = pontoEbulicaoKelvin - 273
+	fmt.Println("O ponto de Ebulição da água de Kelvin para graus Celsius é: ", celsius)
+}
